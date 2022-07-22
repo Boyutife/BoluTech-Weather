e65c19575ba800922921ea32ddf40cbee62099dc
iamBoluwatife <boluwatifeaiyenibe@gmail.com>
2022-07-22 15:31:51 +0100

    i omitted await @ forecast.js:9

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,17 +1,40 @@
 const key = 'e7gRRKizDmsP2hak5y2ADZY9dgFRCTT0';
 
+// // get weather info
+ const getWeather = async (location) =>{
+
+ const base ='http://dataservice.accuweather.com/currentconditions/v1/';
+ const query = `${location}?apikey=${key}`;
+
+ const response = await fetch(base + query);
+ const data = await response.json();
+
+  return data[0];
+
+//  console.log(data);
+
+};
+
+// get city info
 const getCity = async (city) => {
 
   const base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
   const query = `?apikey=${key}&q=${city}`;
 
-  const response = await fetch (base + query);
+  const response = await fetch(base + query);
   const data = await response.json();
 
   return data[0];
+  // console.log(data);
+  
 
 } ;
 
-getCity('lagos')
-  .then(data => console.log(data))
-  .catch(err => console.log(err));
+getCity('lagos').then(data =>{
+  return getWeather(data.Key);})
+  .then(data => {
+    console.log(data);
+  })
+  .catch( err => {
+    console.log(err);
+  });
